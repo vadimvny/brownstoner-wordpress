64e4f1a1961109725ed72c19eaac71d48c2bea23
Poopsik <vadimvny@Vadims-MBP.home>
2015-07-29 11:51:31 -0400

    added mobile sidebar

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div class="row">
-	<div id="main" class="large-9 columns">	
+	<div id="main" class="large-9 columns left">	
 		<?php while ( have_posts() ) : the_post(); ?>
 			<h2><a href="<?php the_permalink();?>"><?php the_title() ?></a></h2>		
 			<p>Written by: 
@@ -8,9 +8,9 @@
 			<?php the_content(__('Continue Reading')); ?>
 			</br>
 		<?php endwhile; ?>
-		<div class="large-3 columns">
-		<?php get_sidebar(); ?>
-	</div>
+	</div>	
+	<div class="large-3 columns right">
+			<?php get_sidebar(); ?>
 	</div>
 </div>	
 <?php get_footer(); ?>
