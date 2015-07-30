7c5baf28a742239de9da07f23e3852bb7f235a2d
Poopsik <vadimvny@Vadims-MBP.home>
2015-07-29 20:44:43 -0400

    completed

--- footer.php
+++ footer.php
@@ -1,15 +1,25 @@
 		
 		</div><!-- container -->
 		<footer>
-			<a href="<?php home_url('/')?>"><img class="logo scale" src="http://cdn.brownstoner.com/wp-content/uploads/2013/06/footerlogo.png" width="265" height="100" alt="Brownstoner"></a>
-			<table>
-				<nav id="ft-primary"><?php wp_nav_menu(); ?></nav>
-				<nav id="ft-secondary">
-					<?php wp_nav_menu( array('container_class' => 'menu-footer',
-					'theme_location' => 'secondary' ) ); 
-					?>
-				</nav>
-			</table>		
+			<div class="row">
+				<div class="large-8 columns">
+					<div class="row">
+						<div class="large-5 columns">
+							<a href="<?php home_url('/')?>"><img class="logo scale" src="http://cdn.brownstoner.com/wp-content/uploads/2013/06/footerlogo.png" width="265" height="100" alt="Brownstoner"></a>
+						</div>
+					<div class="large-2 columns">
+						<nav id="ft-primary"><?php wp_nav_menu(); ?></nav>
+					</div>
+					<div class="large-2 columns">
+						<nav id="ft-secondary">
+							<?php wp_nav_menu( array('container_class' => 'menu-footer',
+							'theme_location' => 'secondary' ) ); 
+							?>
+						</nav>
+					</div>
+				</div>
+				</div>	
+			</div>			
 		</footer>
 	</body>
 </html>
--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,7 @@
 <aside>
+	<img src="https://tpc.googlesyndication.com/simgad/10411299533955301595">
+	</br>
+	<img src="https://tpc.googlesyndication.com/simgad/10411299533955301595">
+
 	<?php dynamic_sidebar( 'right-sidebar' ); ?> 
 </aside>
